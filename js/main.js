a0c4e349b21f14447280100d1a6809c4f69c8f89
mludwig99 <michalludwig99@gmail.com>
2022-09-19 21:19:55 +0200

    Complete project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("dziala")
+const price = document.querySelector("#price");
+const people = document.querySelector("#people");
+const tip = document.querySelector("#tip");
+const error = document.querySelector(".error");
+const countBtn = document.querySelector(".count");
+const costInfo = document.querySelector(".cost-info");
+const cost = document.querySelector(".cost");
+
+const showBill = () => {
+  if (price.value === "" || people.value === "" || tip === "0") {
+    error.textContent = "complete all fields!";
+    costInfo.style.display = "none";
+  } else {
+    error.textContent = "";
+    countBill();
+  }
+};
+const countBill = () => {
+  const newPrice = parseFloat(price.value);
+  const newPeople = parseFloat(people.value);
+  const newTip = parseFloat(tip.value);
+
+  const result = (newPrice + newPrice * newTip) / newPeople;
+  costInfo.style.display = "block";
+
+  cost.textContent = result.toFixed(2);
+};
+
+countBtn.addEventListener("click", showBill);
